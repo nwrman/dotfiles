60933e4171be31275dcf021f721b4130e8abb4e1
Jonathan H. Fernández <nwrman@gmail.com>
2022-03-22 15:05:50 -0600

    Set custom git aliases

--- home/.zsh/common.zsh
+++ home/.zsh/common.zsh
@@ -18,3 +18,40 @@ alias gp="git pull --rebase --autostash"
 alias gu="git reset --soft HEAD~1"
 alias magento="php -d memory_limit=2048M ./bin/magento"
 
+# GIT
+
+alias g='git'
+alias gco='git checkout'
+alias gco='git checkout'
+alias gc='git commit --verbose'
+alias gcp='git cherry-pick --ff'
+alias gf='git fetch'
+alias gfa='git fetch --all'
+alias gfc='git clone'
+alias gp='git pull --rebase --autostash'
+
+alias gl='git log --topo-order --pretty=format:"$_git_log_medium_format"'
+alias gls='git log --topo-order --stat --pretty=format:"$_git_log_medium_format"'
+alias gld='git log --topo-order --stat --patch --full-diff --pretty=format:"$_git_log_medium_format"'
+alias glo='git log --topo-order --pretty=format:"$_git_log_oneline_format"'
+alias glg='git log --topo-order --graph --pretty=format:"$_git_log_oneline_format"'
+alias glb='git log --topo-order --pretty=format:"$_git_log_brief_format"'
+alias glc='git shortlog --summary --numbered'
+
+alias gpu='git push'
+alias gpuF='git push --force'
+
+alias gr='git rebase'
+alias gra='git rebase --abort'
+alias grc='git rebase --continue'
+
+alias gR='git remote --verbose'
+alias gRa='git remote add'
+alias gRx='git remote rm'
+alias gRm='git remote rename'
+alias gRu='git remote update'
+
+alias gst='git status --ignore-submodules=$_git_status_ignore_submodules'
+alias gdiff='git diff --no-ext-diff'
+alias grs='git reset --soft'
+alias grh='git reset --hard'
